79764c2a10d5ba48ffa37f1c2199c59b95c3ac0a
RaghavBatchu <batchuraghavendra07@gmail.com>
2026-01-20 22:33:22 +0530

    Set up Neon Postgres w/Drizzle proper

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 //setting up express server and middleware
 
 import express from 'express';
+import logger from './config/logger.js';
 const app = express();
 
 app.get('/', (req, res) => {
+    logger.info('hello route accessed');
   res.status(200).send('Hello, from Acquisitions!');
 });
 
